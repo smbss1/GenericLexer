f85f7433952a99aca20b6140f4e62e8ab6a61c91
smbss1 <softandsgame@gmail.com>
2021-02-21 15:08:28 +0100

    [REPLACE] Lexer regex to standard c++ regex

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -3,8 +3,8 @@
 #include <vector>
 #include <cstring>
 #include <algorithm>
+#include <regex>
 #include "Lexer.h"
-#include "re.h"
 
 Lexer::Lexer()
 {
@@ -48,36 +48,15 @@ bool Lexer::TokenMatch(Token oToken, const std::string& strString)
     return (oToken.GetText() == strString);
 }
 
-int Lexer::SkipComment(bool long_comment)
-{
-    int line = 0;
-    m_strCurrent += 2;
-    if (long_comment) {
-        while (*m_strCurrent != '*' && *(m_strCurrent + 1) != '/' && *m_strCurrent) {
-            if (*m_strCurrent == '\n' || *m_strCurrent == '\0')
-                ++line;
-            m_strCurrent++;
-        }
-        m_strCurrent++;
-    } else {
-        while (*m_strCurrent != '\n' && *m_strCurrent) {
-            m_strCurrent++;
-        }
-        ++line;
-    }
-
-    return (line);
-}
-
 bool Lexer::Process(const std::string& strText)
 {
     m_strText    = strText;
     oTokenList.clear();
+    m_strList.clear();
 
     if (strText.empty())
         return false;
 
-    bool bError = false;
     m_iLines = 1;
 
     int iLen = 0;
@@ -86,6 +65,7 @@ bool Lexer::Process(const std::string& strText)
     std::vector<StringID>::iterator itTrash;
     int maxLen = 0;
     std::pair<StringID, std::string> defineTarget;
+	// helper::replaceAll(m_strText, "\\\"", "\"", m_strText.size());
 
     while (!m_strText.empty())
     {
@@ -97,45 +77,96 @@ bool Lexer::Process(const std::string& strText)
 		{
 			if (define.second.m_cStart == m_strText[0])
 			{
-				char* current = (char *) m_strText.c_str();
-				const char* start = current;
-				const char* startContent = ++current;
-				while(current && *current != define.second.m_cEnd)
-					current++;
-				oTokenList.emplace_back(define.first, startContent, current, m_iLines);
-				if (*current == define.second.m_cEnd)
-					current++;
-				m_strText.erase(0, std::distance((char*)start, current));
+				m_strText.erase(0, 1);
+				std::string newStr = "";
+				int i = 0;
+				for(i = 0; m_strText[i] != '"'; i++)
+				{
+					if (m_strText[i] == '\\')
+					{
+						switch(m_strText[++i])
+						{
+							case '\\':
+								newStr += '\\';
+								break;
+							case 'n':
+								newStr += '\n';
+								break;
+							case 't':
+								newStr += '\t';
+								break;
+							case 'r':
+								newStr += '\r';
+								break;
+							case '"':
+								newStr += '"';
+								break;
+						}
+					} else
+						newStr += m_strText[i];
+				}
+				oTokenList.emplace_back(define.first, newStr, newStr.size(), m_iLines);
+                m_strList.emplace_back(newStr);
+				m_strText.erase(0, i + 1);
 				break;
 			}
 		}
 
         for (auto& define : m_oAllDefines)
         {
-            // const char* pStart = nullptr;
-            // re.Compile(define.second.c_str());
-            // re_compile(define.second.c_str());
-            int index = re_match(define.second.c_str(), m_strText.c_str(), &iLen);
-            // pStart = re.Search(m_strText.c_str(), &iLen);
-            // std::cout << define.second.c_str() << std::endl;
-
-            if (index == 0)
+            std::smatch match;
+            std::regex reg(define.second);
+            if (std::regex_search(m_strText, match, reg))
             {
-                if (maxLen < iLen)
+                int index = match.position();
+                iLen = match.length();
+                if (index == 0)
                 {
-                    defineTarget = define;
-                    maxLen = iLen;
-                    pText = m_strText.substr(index, maxLen);
+                    if (maxLen < iLen)
+                    {
+                        defineTarget = define;
+                        maxLen = iLen;
+                        pText = m_strText.substr(index, maxLen);
+                    }
                 }
-                // std::cout << define.first << " - " << m_iLines << std::endl;
             }
         }
 
         if (maxLen > 0)
         {
             itTrash = std::find(m_oTrashDefines.begin(), m_oTrashDefines.end(), defineTarget.first);
+			std::string newStr = "";
+			int i = 0;
             if (itTrash == m_oTrashDefines.end())
-                oTokenList.emplace_back(defineTarget.first, pText, maxLen, m_iLines);
+			{
+				for(i = 0; i < maxLen; i++)
+				{
+					if (m_strText[i] == '\\')
+					{
+						switch(m_strText[++i])
+						{
+							case '\\':
+								newStr += '\\';
+								break;
+							case 'n':
+								newStr += '\n';
+								break;
+							case 't':
+								newStr += '\t';
+								break;
+							case 'r':
+								newStr += '\r';
+								break;
+							case '"':
+								newStr += '"';
+								break;
+						}
+					} else
+						newStr += m_strText[i];
+				}
+                oTokenList.emplace_back(defineTarget.first, newStr, i, m_iLines);
+                m_strList.emplace_back(newStr);
+			}
             m_strText.erase(0, maxLen);
             bFound = true;
 
@@ -143,8 +174,13 @@ bool Lexer::Process(const std::string& strText)
                 m_iLines++;
         }
 
-        if (iLen == 1 && !bFound) {
-            oTokenList.emplace_back(StringID(84), "Unexpected Character.", (std::size_t) 21, (std::size_t) m_iLines);
+        if (!bFound) {
+            if (!m_bEnableError) {
+                std::string strSub = m_strText.substr(0, 1);
+                oTokenList.emplace_back(StringID(0), strSub, (std::size_t) 1, (std::size_t) m_iLines);
+                m_strList.emplace_back(strSub);
+            } else
+                oTokenList.emplace_back(StringID(84), "Unexpected Character.", (std::size_t) 21, (std::size_t) m_iLines);
             m_strText.erase(0, 1);
         }
     }
@@ -206,20 +242,6 @@ bool Lexer::Finished() const
 {
     return (oTokenList.end() == oTokenIterator);
 }
-
-void Lexer::AddArea(std::pair<char, char> cRange)
-{
-    // m_oAreas.emplace_back(cRange);
-}
-
-// enum class Format {
-//     TEXT = 0,
-//     PDF = 1000,
-//     OTHER = 2000,
-// };
-// Format f = Format::PDF;
-// int a = f;                         // error
-// int b = static_cast<int>(f);
 
 void Lexer::Define(const std::string& strId, const std::string& strRegex, bool bAddInTrash)
 {
